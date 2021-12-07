59baf21e033dcf5b166a64b8591d9cffaf95d091
hyunsoo93049 <sid1130@naver.com>
2021-12-08 05:38:13 +0900

    DDR게임 코드업로드 03(김현수) beat.py-> librosa를 이용하여 비트프레임, 타임스탬프 찾고 csv파일에 저장

--- DDRGAME/beat.py
+++ DDRGAME/beat.py
@@ -3,8 +3,12 @@ from pydub.utils import make_chunks
 import pyaudio
 import librosa
 import pygame
-
+import numpy as np
+import pandas as pd
 filename = "just fine.mp3"
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-#beat_frames = beat_frames * 1000 #m to ms
-beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+y, sr = librosa.load(filename)
+tempo, beat_frames = librosa.beat.beat_track(y=y,sr=sr)
+beat_times=librosa.frames_to_time(beat_frames, sr=sr)
+print(type(beat_times))
+
+pd.DataFrame(beat_times).to_csv('beatdata.csv')
