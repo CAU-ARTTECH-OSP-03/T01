f7251b640901a48e0112d32a888f3e2fef1325a3
LR429 <starry_wizard@naver.com>
2021-12-10 00:43:21 +0900

    메뉴 코드 업로드01(이령)-startmenu 추가

---
+++ MENU/startmenu.py
@@ -0,0 +1,27 @@
+import pygame
+pygame.init()
+
+SCREEN_HEIGHT = 560
+SCREEN_WIDTH = 1000
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+Background = pygame.image.load("IMG/MenuBackground.png")
+
+def StartMenu():
+    run = True
+
+    while run:
+        font = pygame.font.Font('NanumGothic.ttf', 30)
+        text = font.render("Press any Key to Start", True, (0, 0, 0))
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+        SCREEN.blit(Background, (0, 0))
+        SCREEN.blit(text, textRect)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                pygame.quit()
